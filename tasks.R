263142b326a0bcbdf53d1d773c07164c2d032362
vasu258 <74041654+vasu258@users.noreply.github.com>
2020-11-06 18:02:12 +0530

    Update tasks.R

--- tasks.R
+++ tasks.R
@@ -5,9 +5,12 @@ library(readxl)
 mydata=read_xlsx(file.choose())
 View(mydata)
 str(mydata)
+
+#converting scores variable into interger type
 mydata$Scores=as.integer(mydata$Scores)
 summary(mydata)
 
+#data visualization
 library(ggplot2)
 ggplot(mydata,aes(Hours,Scores))+geom_point()+ggtitle("Scores vs Hours")
 barplot(Hours,Scores,data=mydata)
@@ -15,8 +18,10 @@ plot(mydata,col=" blue",main="Scores vs Hours")
 abline(model,col="red")
 plot(model)
 
+# plot for identifying missing values in the dataset
 library(DataExplorer)
 plot_missing(mydata)
+
 # Splitting the dataset into training data and test data
 #install.packages('caTools')
 library(caTools)
@@ -25,15 +30,23 @@ train=mydata[split==1,]
 test=mydata[split==2,]
 dim(train)
 dim(test)
+
 # Fitting Simple Linear Regression to the Training set
 model=lm(Scores~Hours,data=train)
 summary(model)
+
+#predicting the test set output
 pred=predict(model,train)
 pred
 dataF=data.frame(train,pred)
 dataF
+
+
+#comparing actual scores with predicted scores
 comparison=data.frame(actual_scores=train$Scores,predcited_scores=pred)
 comparison
+
+#calculating root mean square error onn train and test data
 library(ModelMetrics)
 rmse(train$Scores,pred)
 pred_test=predict(model,test)
@@ -43,7 +56,10 @@ df
 df1=data.frame(actual_scores=test$Scores,predicted_scores=pred_test)
 df1
 rmse(test$Scores,pred_test)
+
 # based on the coefficient of the model
 # predict the score of the student when he studies 9.25 hrs/day
 coef(model)[1] + 9.25*coef(model)[2]
+
+#student who studied for 9.25 hrs will score 93.04906 
 
